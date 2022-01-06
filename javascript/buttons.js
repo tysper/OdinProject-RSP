b0e632d028d833696240cd0cdbbda769cd78b9ba
Pedro Nunes <pedropaulo_nunes@outlook.com>
2022-01-06 14:27:57 -0300

    Creating modal window and transitions

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -5,13 +5,12 @@ const helpButton = document.querySelector(".help-btn");
 const backButton = document.querySelector(".back-btn");
 const initialScreen = document.querySelector(".main-screen");
 const gameScreen = document.querySelector(".start-screen");
+const closeBtn = document.querySelector(".");
 
 startButton.addEventListener("click", (a) => {
-  initialScreen.classList.toggle("hidden");
-  gameScreen.classList.toggle("hidden");
+  gameScreen.scrollIntoView({ behavior: "smooth", block: "center" });
 });
 
 backButton.addEventListener("click", (a) => {
-  initialScreen.classList.toggle("hidden");
-  gameScreen.classList.toggle("hidden");
+  initialScreen.scrollIntoView({ behavior: "smooth", block: "center" });
 });
