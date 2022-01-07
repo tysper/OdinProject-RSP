52927f9a59b1db414bfa1afbf2a6a13a1ff89323
Pedro Nunes <pedropaulo_nunes@outlook.com>
2022-01-07 13:41:18 -0300

    finished animations and loading

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,16 +1,54 @@
 "use strict";
 
+const loadingScreen = document.querySelector(".loading");
 const startButton = document.querySelector(".start-btn");
 const helpButton = document.querySelector(".help-btn");
 const backButton = document.querySelector(".back-btn");
+const backButtonHelp = document.querySelector(".back-btn-help");
+
 const initialScreen = document.querySelector(".main-screen");
 const gameScreen = document.querySelector(".start-screen");
-const closeBtn = document.querySelector(".");
+const helpScreen = document.querySelector(".help-screen");
+
+initialScreen.scrollIntoView();
+
+function hideElement(element) {
+  element.style.visibility = "hidden";
+  return element;
+}
+
+function showElement(element) {
+  element.style.visibility = "visible";
+  return element;
+}
+
+hideElement(helpScreen);
+hideElement(gameScreen);
+
+setTimeout(() => {
+  loadingScreen.classList.add("hidden");
+}, 3000);
 
 startButton.addEventListener("click", (a) => {
+  hideElement(initialScreen);
   gameScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  showElement(gameScreen);
 });
 
 backButton.addEventListener("click", (a) => {
+  hideElement(gameScreen);
   initialScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  showElement(initialScreen);
+});
+
+backButtonHelp.addEventListener("click", (a) => {
+  hideElement(helpScreen);
+  initialScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  showElement(initialScreen);
+});
+
+helpButton.addEventListener("click", (a) => {
+  hideElement(initialScreen);
+  helpScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  showElement(helpScreen);
 });
