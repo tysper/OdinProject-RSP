0318790d32ef8aa16d981eb40f4f73aa2020da05
Pedro Nunes <pedropaulo_nunes@outlook.com>
2022-01-08 02:54:08 -0300

    bug fixing

--- javascript/buttons.js
+++ javascript/buttons.js
@@ -1,15 +1,18 @@
 "use strict";
 
+// ====BUTTONS =====
 const loadingScreen = document.querySelector(".loading");
 const startButton = document.querySelector(".start-btn");
 const helpButton = document.querySelector(".help-btn");
 const backButton = document.querySelector(".back-btn");
 const backButtonHelp = document.querySelector(".back-btn-help");
 
+// ====SCREENS =====
 const initialScreen = document.querySelector(".main-screen");
 const gameScreen = document.querySelector(".start-screen");
 const helpScreen = document.querySelector(".help-screen");
 
+// ====FUNCTIONS =====
 function hideElement(element) {
   element.style.visibility = "hidden";
   return element;
@@ -20,38 +23,62 @@ function showElement(element) {
   return element;
 }
 
+// ====EVENT LISTENERS ====
+startButton.addEventListener("click", (a) => {
+  // hideElement(initialScreen);
+  // gameScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  // showElement(gameScreen);
+  initialScreen.classList.toggle("scrollToMain");
+  helpScreen.classList.toggle("scrollToMain");
+  gameScreen.classList.toggle("scrollToMain");
+  initialScreen.classList.toggle("scrollToGame");
+  helpScreen.classList.toggle("scrollToGame");
+  gameScreen.classList.toggle("scrollToGame");
+});
+
+backButton.addEventListener("click", (a) => {
+  // hideElement(gameScreen);
+  // initialScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  // showElement(initialScreen);
+  initialScreen.classList.toggle("scrollToGame");
+  helpScreen.classList.toggle("scrollToGame");
+  gameScreen.classList.toggle("scrollToGame");
+  initialScreen.classList.toggle("scrollToMain");
+  helpScreen.classList.toggle("scrollToMain");
+  gameScreen.classList.toggle("scrollToMain");
+});
+
+backButtonHelp.addEventListener("click", (a) => {
+  // hideElement(helpScreen);
+  // initialScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  // showElement(initialScreen);
+  initialScreen.classList.toggle("scrollToMain");
+  helpScreen.classList.toggle("scrollToMain");
+  gameScreen.classList.toggle("scrollToMain");
+});
+
+helpButton.addEventListener("click", (a) => {
+  // hideElement(initialScreen);
+  // helpScreen.scrollIntoView({ behavior: "smooth", block: "center" });
+  // showElement(helpScreen);
+  initialScreen.classList.toggle("scrollToMain");
+  helpScreen.classList.toggle("scrollToMain");
+  gameScreen.classList.toggle("scrollToMain");
+  initialScreen.classList.toggle("scrollToHelp");
+  helpScreen.classList.toggle("scrollToHelp");
+  gameScreen.classList.toggle("scrollToHelp");
+  setTimeout(() => {
+    initialScreen.classList.toggle("scrollToHelp");
+    helpScreen.classList.toggle("scrollToHelp");
+    gameScreen.classList.toggle("scrollToHelp");
+  }, 300);
+});
+
+// ====INITIAL CONFIG ====
 setTimeout(() => {
   loadingScreen.classList.add("hidden");
 }, 3000);
 
-// function scrollScreen(x, y) {
-//   document.querySelector(".game-display").scrollTo(x, y);
-// }
-
-startButton.addEventListener("click", (a) => {
-  hideElement(initialScreen);
-  gameScreen.scrollIntoView({ behavior: "smooth", block: "center" });
-  showElement(gameScreen);
-});
-
-backButton.addEventListener("click", (a) => {
-  hideElement(gameScreen);
-  initialScreen.scrollIntoView({ behavior: "smooth", block: "center" });
-  showElement(initialScreen);
-});
-
-backButtonHelp.addEventListener("click", (a) => {
-  hideElement(helpScreen);
-  initialScreen.scrollIntoView({ behavior: "smooth", block: "center" });
-  showElement(initialScreen);
-});
-
-helpButton.addEventListener("click", (a) => {
-  hideElement(initialScreen);
-  helpScreen.scrollIntoView({ behavior: "smooth", block: "center" });
-  showElement(helpScreen);
-});
-
-initialScreen.scrollIntoView();
-hideElement(helpScreen);
-hideElement(gameScreen);
+// initialScreen.scrollIntoView();
+// hideElement(helpScreen);
+// hideElement(gameScreen);
